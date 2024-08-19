b144ba1c2451740542e05d4e556f670c54410610
Odyhibit <josh.2990@gmail.com>
2024-08-18 23:11:13 -0500

    testing pandas .loc with boolean values seems to work pretty well

--- steganography/pandas_idea.py
+++ steganography/pandas_idea.py
@@ -11,15 +11,16 @@ def get_image_data_bool(filename: str):
     """
     img = Image.open(filename)
     img_array = np.array(img.getdata(), dtype="uint8")
-    img_bool = np.unpackbits(img_array, axis=1, bitorder="little")
-    return img_bool.T
+    print(f"Initial np array shape {img_array.shape}")
+    img_bool = np.unpackbits(img_array, axis=1, bitorder="big")
+    return img.width, img.height, img_bool.T
 
 
 cover_image = "../test_images/test.png"
 lsb_checkboxes = [False, False, False, False, False, False, False, True,
                   False, False, False, False, False, False, False, True,
                   False, False, False, False, False, False, False, True]
-cover_img = get_image_data_bool(cover_image)
+width, height, cover_img = get_image_data_bool(cover_image)
 print(cover_img.T.shape)
 print(cover_img.T[0:8, 0:24])
 
@@ -28,7 +29,7 @@ lsb = pd_idea.loc[lsb_checkboxes]
 lots_o_ones = np.ones((3, 100800), dtype="uint8")
 pd_idea.loc[lsb_checkboxes] = lots_o_ones
 
-print("The lsb ")
+print("The lsb ", len(lsb))
 print(lsb)
 print("With all values replaced with ones")
 np_answer = np.packbits(pd_idea.T, axis=1)
@@ -36,3 +37,21 @@ np_answer = np.packbits(pd_idea.T, axis=1)
 print(pd_idea)
 print(lots_o_ones.shape)
 print(np_answer.shape)
+with open("test_panda_1.png", "wb") as output_file:
+    output_img = Image.new("RGB", (width, height))
+    output_array = [tuple(i) for i in np_answer]
+    output_img.putdata(output_array)
+    output_img.save(output_file)
+
+"""
+# This creates an image with a width of 3 and a height of 100800
+stego_image2 = Image.fromarray(np_answer)
+stego_image2.save("test_panda_2.png")
+
+"""
+
+img_stego = Image.open("test_panda_1.png")
+stego_array = np.array(img_stego.getdata())
+print(stego_array[0])
+print(stego_array.shape)
+print(stego_array[0:250, 0:3])
